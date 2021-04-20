9228154fddbbb886f93886afa871ef3c55728f63
Jorge Urrutia <jorge.urrutia@arkade.com.co>
2021-04-20 16:18:33 -0500

    Cambios de estilos

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,13 +4,15 @@ import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
 import '../assets/styles/App.scss';
+import '../assets/styles/Media.scss';
 
 const App = () => (
     <div className="App">
         <Header />
         <Search />
-        <Categories >
+        <Categories title="Mi Lista">
             <Carousel >
                 <CarouselItem />
                 <CarouselItem />
@@ -18,6 +20,18 @@ const App = () => (
                 <CarouselItem />
             </Carousel>
         </Categories>
+        <Categories title="Tendencias">
+            <Carousel >
+                <CarouselItem />
+                <CarouselItem />                
+            </Carousel>
+        </Categories>
+        <Categories title="Originales de Platzy Video">
+            <Carousel >
+                <CarouselItem />                
+            </Carousel>
+        </Categories>
+        <Footer />
         
     </div>
 );
